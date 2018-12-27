55ba8e7e301f1fe0aee26e38979d165073679e28
Gallaugher <john.gallaugher@bc.edu>
2018-12-27 16:15:12 -0500

    created get_IR_code function
    Isolated code, reduced global variables

--- code.py
+++ code.py
@@ -35,41 +35,38 @@ red_led.direction = digitalio.Direction.OUTPUT
 
 pulsein = pulseio.PulseIn(board.REMOTEIN, maxlen=120, idle_state=True)
 decoder = adafruit_irremote.GenericDecode()
-received_code = bytearray(4)
-last_command = None
 print("Everything configured & ready to run!")
 
-while True:
+def get_IR_code():
     red_led.value = False  # turn off red_led at D13
     try:
         pulses = decoder.read_pulses(pulsein)
     except MemoryError as e:
         print("Memory error: ", e)
-        continue
+        return None
     red_led.value = True  # Got this far? Show no errors & light red_led
-    command = None
     try:
-        code = decoder.decode_bits(pulses, debug=False)
-        if len(code) > 3:
-            command = code[2]
-        print("Decoded:", code)
+        ir_code = decoder.decode_bits(pulses, debug=False)
+        print("Decoded:", ir_code)
     except adafruit_irremote.IRNECRepeatException as e:  # unusual short code!
         print("Repeated Code Error:", e.args)
-        command = last_command
+        return None
     except adafruit_irremote.IRDecodeException as e:  # failed to decode
         print("Failed to decode:", e.args)
+        return None
     except MemoryError as e:
         print("Memory error: ", e.args)
+        return None
+    red_led.value = False  # turn off red_led since command successfully received.
+    print("NEC Infrared code received: ", ir_code)
+    return ir_code
 
-    if not command:
+while True:
+    received_code = get_IR_code()
+    if not received_code: # if no proper code received, restart while loop
         continue
-    last_command = command
-    red_led.value = False
-
-    received_code = code
-    print("NEC Infrared code received: ", received_code)
-        
-    if received_code == [255, 0, 157, 98]:
+    
+    if received_code == [255, 0, 157, 98]: 
         print("Go Forward!")  # Vol +
         motor_left.throttle = -0.5  # full speed!
         motor_right.throttle = 0.5
@@ -89,4 +86,3 @@ while True:
         print("Turn Right!")  # FWD
         motor_left.throttle = -0.3  # right
         motor_right.throttle = -0.3
-    
